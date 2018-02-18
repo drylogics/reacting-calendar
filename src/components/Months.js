3b93b915b7db00431ed838865fbbbf787cb16bf4
Saravana Kumar <saravana.kumar@drylogics.com>
2018-02-18 20:13:01 +0530

    Refactored months and years components

--- src/components/Months.js
+++ src/components/Months.js
@@ -1,37 +1,18 @@
 import React, { Component } from 'react'
-import moment from 'moment';
-
 
 export class Months extends Component {
 
   render() {
-    const FirstRow = () => (
-      <tr>
-      {
-        [...Array(4).keys()].map((i) =>  <td>{(this.props.currentState.firstSunday.startOf('year').clone()).add(i,'months').format('MMM')}</td> )
-      }
-      </tr>
-    );
-    const SecondRow = () => (
-      <tr>
-      {
-        [...Array(4).keys()].map((i) =>  <td>{(this.props.currentState.firstSunday.startOf('year').clone()).add(i+4,'months').format('MMM')}</td> )
-      }
-      </tr>
-    );
-    const ThirdRow = () => (
-      <tr>
-      {
-        [...Array(4).keys()].map((i) =>  <td>{(this.props.currentState.firstSunday.startOf('year').clone()).add(i+8,'months').format('MMM')}</td> )
-      }
-      </tr>
-    );
     return(
-      // <p>{this.state.firstSunday.format('DD')}</p>
       <tbody>
-        <FirstRow />
-        <SecondRow />
-        <ThirdRow />
+      {
+        [...Array(3).keys()].map((j) =>
+          <tr>
+          {
+            [...Array(4).keys()].map((i) => <td>{(this.props.currentState.firstSunday.startOf('year').clone()).add(i+(j*4),'months').format('MMM')}</td> )
+          }
+          </tr>)
+      }
       </tbody>
     );
   }
--- src/components/Years.js
+++ src/components/Years.js
@@ -1,36 +1,18 @@
 import React, { Component } from 'react'
-import moment from 'moment';
 
 export class Years extends Component {
 
   render() {
-    const FirstRow = () => (
-      <tr>
-      {
-        [...Array(4).keys()].map((i) =>  <td>{(this.props.currentState.firstSunday.startOf('year').clone()).add(i - 9,'years').format('YYYY')}</td> )
-      }
-      </tr>
-    );
-    const SecondRow = () => (
-      <tr>
-      {
-        [...Array(4).keys()].map((i) =>  <td>{(this.props.currentState.firstSunday.startOf('year').clone()).add(i - 5,'years').format('YYYY')}</td> )
-      }
-      </tr>
-    );
-    const ThirdRow = () => (
-      <tr>
-      {
-        [...Array(4).keys()].map((i) =>  <td>{(this.props.currentState.firstSunday.startOf('year').clone()).add(i - 1,'years').format('YYYY')}</td> )
-      }
-      </tr>
-    );
     return(
-      // <p>{this.state.firstSunday.format('DD')}</p>
       <tbody>
-        <FirstRow />
-        <SecondRow />
-        <ThirdRow />
+      {
+        [...Array(3).keys()].map((j) =>
+          <tr>
+          {
+            [...Array(4).keys()].map((i) => <td>{(this.props.currentState.firstSunday.startOf('year').clone()).add(i + ((j*4) - 9),'years').format('YYYY')}</td> )
+          }
+          </tr>)
+      }
       </tbody>
     );
   }
